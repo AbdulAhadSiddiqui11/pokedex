937795fcec3e856f1246af505141671ff7a6e47b
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-01-24 00:13:31 +0530

    UI fixes 2

--- src/components/FetchBox.tsx
+++ src/components/FetchBox.tsx
@@ -8,7 +8,7 @@ const FetchBox = ({ handleSubmit }) => {
     <div className="p-3 flex justify-center items-center flex-wrap">
       <input
         type="number"
-        className="border border-gray-600 h-12 px-5 mx-2 rounded-lg py-1"
+        className="border border-gray-600 h-12 px-5 mx-2 rounded-lg my-1"
         value={firstIdx || ""}
         onChange={(e) => setFirstIdx(Number(e.target.value))}
         placeholder="First Pokemon ID?"
@@ -16,14 +16,14 @@ const FetchBox = ({ handleSubmit }) => {
       />
       <input
         type="number"
-        className="border border-gray-600 h-12 px-5 mx-2 rounded-lg py-1"
+        className="border border-gray-600 h-12 px-5 mx-2 rounded-lg my-1"
         value={lastIdx || ""}
         onChange={(e) => setLastIdx(Number(e.target.value))}
         placeholder="Last Pokemon ID?"
         pattern="[0-9]*"
       />
       <button
-        className="bg-blue-900 text-white h-12 px-5 my-2 rounded-lg hover:bg-blue-950 py-1"
+        className="bg-blue-900 text-white h-12 px-5 my-2 rounded-lg hover:bg-blue-950"
         onClick={() => handleSubmit(firstIdx, lastIdx)}
       >
         Get Pokemon!
