68d3b5901503d8024e2821f0bd97a57a6f6fa359
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2024-01-27 03:19:22 +0530

    UX overhaul

--- src/components/FetchBox.tsx
+++ src/components/FetchBox.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC, useState, useRef } from "react";
 
 interface IFetchBoxProps {
   handleSubmit: (newFirstPokeIdx: number, newLastPokeIdx: number) => void;
@@ -8,6 +8,9 @@ const FetchBox: FC<IFetchBoxProps> = ({ handleSubmit }) => {
   const [firstIdx, setFirstIdx] = useState<number>(0);
   const [lastIdx, setLastIdx] = useState<number>(0);
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const secondIdxRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="p-3 flex justify-center items-center flex-wrap">
       <input
@@ -15,6 +18,8 @@ const FetchBox: FC<IFetchBoxProps> = ({ handleSubmit }) => {
         className="border border-gray-600 h-12 px-5 mx-2 rounded-lg my-1"
         value={firstIdx || ""}
         onChange={(e) => setFirstIdx(Number(e.target.value))}
+        onBlur={() => secondIdxRef.current?.focus()}
+        onKeyDown={(e) => { if(e.key === 'Enter') secondIdxRef.current?.focus() }}
         placeholder="First Pokemon ID?"
         pattern="[0-9]*"
       />
@@ -23,12 +28,16 @@ const FetchBox: FC<IFetchBoxProps> = ({ handleSubmit }) => {
         className="border border-gray-600 h-12 px-5 mx-2 rounded-lg my-1"
         value={lastIdx || ""}
         onChange={(e) => setLastIdx(Number(e.target.value))}
+        onBlur={() => buttonRef.current?.focus()}
+        onKeyDown={(e) => {if(e.key === 'Enter') handleSubmit(firstIdx, lastIdx)}}
         placeholder="Last Pokemon ID?"
         pattern="[0-9]*"
+        ref={secondIdxRef}
       />
       <button
         className="bg-blue-900 text-white h-12 px-5 my-2 rounded-lg hover:bg-blue-950"
         onClick={() => handleSubmit(firstIdx, lastIdx)}
+        ref={buttonRef}
       >
         Get Pokemon!
       </button>
